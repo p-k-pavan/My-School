708fe9faafe9871fe9aae32c9d90de9bb6575d5c
p-k-pavan <pkpavan2003@gmail.com>
2026-06-15 17:37:55 +0530

    Protected Route

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -2,23 +2,23 @@ import { useState } from 'react'
 import { Route, Routes } from 'react-router'
 import Login from './pages/auth/Login'
 import DashBoard from "./pages/dashboard"
+import ProtectedRoute from './layout/ProtectedRoute'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
-        <Routes>
-          <Route path="/" element={<Login />} />
-        </Routes>
+      <Routes>
 
-         <Routes>
+        <Route path="/" element={<Login />} />
+
+
+        <Route element={<ProtectedRoute />}>
           <Route path="/dashboard" element={<DashBoard />} />
-        </Routes>
-      
+        </Route>
 
-      
+      </Routes>
     </>
   )
 }
