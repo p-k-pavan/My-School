8ef9f7e4691281fddfeab9c9cc93d45264b45258
p-k-pavan <pkpavan2003@gmail.com>
2026-06-15 10:15:27 +0530

    Dashboard Basic Setup

---
+++ dashboard/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+        <div className="w-full h-full">
+          <h1 className='w-full h-full text-center my-auto underline font-extrabold text-4xl'>Dashboard</h1>
+        </div>
+      
+
+      
+    </>
+  )
+}
+
+export default App
