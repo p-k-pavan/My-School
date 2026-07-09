00bfe09d1318889747c2257167c3d92aade2085e
p-k-pavan <pkpavan2003@gmail.com>
2026-07-09 10:30:11 +0530

    firebase production setup

--- service/src/config/firebase.js
+++ service/src/config/firebase.js
@@ -1,5 +1,18 @@
 import { initializeApp, cert } from "firebase-admin/app";
-import serviceAccount from "./serviceAccountKey.json" with { type: "json" };
+import { readFileSync } from "fs";
+import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+let serviceAccount;
+
+if (!serviceAccount) {
+  const serviceAccountPath =
+    process.env.GOOGLE_APPLICATION_CREDENTIALS ||
+    path.join(import.meta.dirname, "./serviceAccountKey.json");
+  serviceAccount = JSON.parse(readFileSync(serviceAccountPath, "utf8"));
+}
 
 const app = initializeApp({
   credential: cert(serviceAccount)
